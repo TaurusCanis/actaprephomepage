dd70cfc7467d4f903e626b601def11f1f6d83cfe
Taurus Canis <acdole09@gmail.com>
2021-02-06 19:26:21 -0500

    removed extra / from acta_home/urls.py

--- acta_home/urls.py
+++ acta_home/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('acta_homepage.urls')),
+    path('', include('acta_homepage.urls')),
     path('admin/', admin.site.urls),
 ]
