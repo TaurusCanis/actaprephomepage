b98e7a550b57b81a93caf29d6fb19e09eab8b5e3
Taurus Canis <acdole09@gmail.com>
2021-02-10 10:44:59 -0500

    fixed exception to submit_message in views.py

--- acta_homepage/views.py
+++ acta_homepage/views.py
@@ -26,7 +26,7 @@ def submit_message(request):
         )
         messages.add_message(request, messages.SUCCESS, 'Thank you. Your message has successfully been submitted.')
         return redirect("index")
-    except Exception, e:
+    except Exception as e:
         print("EXCEPTION: ", e)
         messages.add_message(request, messages.ERROR, 'There was an error sending your message. Please try again.')
         
